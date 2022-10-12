b010edef87ed7d178170767152bc8dc41af86cbb
mahendrakhamalakbar21510020 <90587995+mahendrakhamalakbar21510020@users.noreply.github.com>
2022-10-12 20:59:05 +0700

    Create main2.java

---
+++ no3/main2.java
@@ -0,0 +1,11 @@
+public class main2 {
+    public static void main(String[] args) {
+        for (int i = 1;i <=5*2;i += 2){
+            for (int j = 1;j <=i;j++){
+                j++;
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}

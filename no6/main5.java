c836f37160c1504543f9dbfb96d25c956cf38618
mahendrakhamalakbar21510020 <90587995+mahendrakhamalakbar21510020@users.noreply.github.com>
2022-10-12 21:19:38 +0700

    Update main5.java

--- no6/main5.java
+++ no6/main5.java
@@ -1,9 +1,10 @@
 public class main5 {
     public static void main(String[] args) {
         int angka = 3;
-        for (int i = 1; i<=4;i++){
-            angka += angka*angka;
+        int hasil =angka;
+        for (int i = 1; i<4;i++){
+            hasil *= angka;
         }
-        System.out.println(angka);
+        System.out.println(hasil);
     }
 }

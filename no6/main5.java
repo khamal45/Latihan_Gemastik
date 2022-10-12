755926982eb790c7bce0246a1004b3c76a0d26a2
mahendrakhamalakbar21510020 <90587995+mahendrakhamalakbar21510020@users.noreply.github.com>
2022-10-12 21:14:47 +0700

    Create main5.java

---
+++ no6/main5.java
@@ -0,0 +1,9 @@
+public class main5 {
+    public static void main(String[] args) {
+        int angka = 3;
+        for (int i = 1; i<=4;i++){
+            angka += angka*angka;
+        }
+        System.out.println(angka);
+    }
+}
